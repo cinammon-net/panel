e7ec8c32ccb88e606d9c8bd7973f2c1d40d7db0f
lz20r <zhiyaonaiara@gmail.com>
2025-05-30 02:05:09 +0000

    feat: agregar archivo PermissionGroups con grupos de permisos y acciones

---
+++ app/Helpers/PermissionGroups.php
@@ -0,0 +1,15 @@
+return [
+'Api Key' => ['view', 'view list', 'create', 'update', 'delete'],
+'Database Host' => ['view', 'view list', 'create', 'update', 'delete'],
+'Database' => ['view', 'view list', 'create', 'update', 'delete'],
+'Egg' => ['view', 'view list', 'create', 'update', 'delete', 'import', 'export'],
+'Mount' => ['view', 'view list', 'create', 'update', 'delete'],
+'Node' => ['view', 'view list', 'create', 'update', 'delete'],
+'Role' => ['view', 'view list', 'create', 'update', 'delete'],
+'Server' => ['view', 'view list', 'create', 'update', 'delete'],
+'User' => ['view', 'view list', 'create', 'update', 'delete'],
+'Webhook' => ['view', 'view list', 'create', 'update', 'delete'],
+'Settings' => ['view', 'update'],
+'Health' => ['view'],
+'Activity' => ['see tips'],
+];
